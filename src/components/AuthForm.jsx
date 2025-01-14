69318769138ae9919592eb8a1abc1ce936a40f1b
Ali Gohar <aligoharkp@gmail.com>
2025-01-14 22:46:03 +0500

    auth page ready

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,10 +1,23 @@
+import logoImage from '../../public/images/logo1.png';
+import namalImage from '../../public/images/namal.png';
 import { useState } from 'react';
 import { auth } from '../firebase/firebaseConfig';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification } from 'firebase/auth';
-
+import {
+    Grid,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    TextField,
+    Button,
+    Typography,
+    Box,
+    Container,
+    Avatar,
+    FormControl,
+} from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import { TextField, Button, Typography, Container, Box, FormControl, Avatar, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
-import logoImage from '../../public/images/logo1.png';
 
 const AuthForm = () => {
     const [email, setEmail] = useState("");
@@ -57,60 +70,108 @@ const AuthForm = () => {
     const handleMethodChange = () => setIsSignUpActive(!isSignUpActive);
 
     return (
-        <>
-            <Container maxWidth='sm' sx={{ mt: 8 }}>
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        paddingTop: 4,
-                        paddingBottom: 7,
-                        paddingLeft: 4,
-                        paddingRight: 4,
-                        border: '1px solid #ccc',
-                        borderRadius: 2,
-                        marginLeft: 3,
-                        width: '80%'
-                    }}
-                >
-                    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                        <Avatar alt="Logo" src={logoImage} sx={{ width: 130, height: 130 }} />
-                    </Box>
-                    <Typography variant="h4" gutterBottom>
-                        {isSignUpActive ? 'Sign Up' : 'Sign In'}
-                    </Typography>
-                    <form>
-                        <FormControl fullWidth margin="normal">
-                            <TextField label="Email" type="email" value={email} onChange={handleEmailChange} required />
-                        </FormControl>
-                        <FormControl fullWidth margin="normal">
-                            <TextField label="Password" type="password" value={password} onChange={handlePasswordChange} required />
-                        </FormControl>
-                        <Button
-                            type="button"
-                            variant="contained"
-                            color="primary"
-                            onClick={isSignUpActive ? handleSignUp : handleSignIn}
-                            fullWidth
-                            sx={{ marginTop: 2 }}
-                        >
+        <Grid container style={{ minHeight: '100vh', minWidth: '100vw' }}>
+            {/* Left Section - Image */}
+            <Grid
+                item
+                xs={12}
+                md={6}
+                style={{
+                    backgroundImage: `url(${namalImage})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    height: '100vh',
+                    backgroundColor: '#d3d3d3',
+                    transition: 'transform 0.3s ease-in-out',
+                }}
+                className="hover-zoom"
+            />
+
+            {/* Right Section - Form */}
+            <Grid
+                item
+                xs={12}
+                md={6}
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    padding: '0 20px',
+                    backgroundColor: '#f7f7f7',
+                }}
+            >
+                <Container maxWidth="sm" sx={{ mt: 1 }}>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            paddingTop: 4,
+                            paddingBottom: 7,
+                            paddingLeft: 4,
+                            paddingRight: 4,
+                            marginLeft: 3,
+                            width: '80%',
+                        }}
+                    >
+                        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mb: 2 }}>
+                            <Avatar alt="Logo" src={logoImage} sx={{ width: 130, height: 130 }} />
+                        </Box>
+                        <Typography variant="h4" gutterBottom>
                             {isSignUpActive ? 'Sign Up' : 'Sign In'}
-                        </Button>
-                        {error && <Typography color="error" sx={{ marginTop: 2 }}>{error}</Typography>}
-                        <Button
-                            type="button"
-                            color="secondary"
-                            onClick={handleMethodChange}
-                            fullWidth
-                            sx={{ marginTop: 2 }}
-                        >
-                            {isSignUpActive ? 'Already have an account? Sign In' : 'Do not have an account? Sign Up'}
-                        </Button>
-                    </form>
-                </Box>
-            </Container>
+                        </Typography>
+                        <form>
+                            <FormControl fullWidth margin="normal">
+                                <TextField
+                                    label="Email"
+                                    name="email"
+                                    type="email"
+                                    value={email}
+                                    onChange={handleEmailChange}
+                                    required
+                                />
+                            </FormControl>
+                            <FormControl fullWidth margin="normal">
+                                <TextField
+                                    label="Password"
+                                    name="password"
+                                    type="password"
+                                    value={password}
+                                    onChange={handlePasswordChange}
+                                    required
+                                />
+                            </FormControl>
+                            <Button
+                                type="button"
+                                variant="contained"
+                                color="primary"
+                                fullWidth
+                                sx={{ marginTop: 2, p:1,fontSize:16 }}
+                                onClick={isSignUpActive ? handleSignUp : handleSignIn}
+                            >
+                                {isSignUpActive ? 'Sign Up' : 'Sign In'}
+                            </Button>
+                            {error && (
+                                <Typography color="error" sx={{ marginTop: 2 }}>
+                                    {error}
+                                </Typography>
+                            )}
+                            <Button
+                                type="button"
+                                color="secondary"
+                                onClick={handleMethodChange}
+                                fullWidth
+                                sx={{ marginTop: 2 }}
+                            >
+                                {isSignUpActive
+                                    ? 'Already have an account? Sign In'
+                                    : 'Do not have an account? Sign Up'}
+                            </Button>
+                        </form>
+                    </Box>
+                </Container>
+            </Grid>
             <Dialog open={openSuccessDialog} onClose={() => setOpenSuccessDialog(false)}>
                 <DialogTitle>Success</DialogTitle>
                 <DialogContent>
@@ -124,7 +185,7 @@ const AuthForm = () => {
                     </Button>
                 </DialogActions>
             </Dialog>
-        </>
+        </Grid>
     );
 };
 
